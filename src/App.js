9fbcc86a6ea8e4a0a686f2b21126bb023f63adbe
defy <8841170+defydef@users.noreply.github.com>
2023-10-18 08:32:36 +0700

    Demonstrate Higher Order Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { faker } from "@faker-js/faker";
 import "./styles.css";
+import withToggles from "./HOC";
 
 const products = Array.from({ length: 20 }, () => {
   return {
@@ -74,12 +75,24 @@ function List({ title, items, render }) {
   );
 }
 
+// LATER: Let's say we got this component from a 3rd-party library, and can't change it. But we still want to add the 2 toggle functionalities to it
+function ProductList({ title, items }) {
+  return (
+    <ul className="list">
+      {items.map((product) => (
+        <ProductItem key={product.productName} product={product} />
+      ))}
+    </ul>
+  );
+}
+
+const ProductListWithToggle = withToggles(ProductList);
+
 export default function App() {
   return (
     <div>
-      <h1>Render Props Demo</h1>
-
-      <div className="col-2">
+      {/*<h1>Render Props Demo</h1>
+       <div className="col-2">
         <List
           title="Products"
           items={products}
@@ -98,18 +111,12 @@ export default function App() {
             />
           )}
         />
+      </div> */}
+      <h1>Higher Order Components Demo</h1>
+      <div className="col-2">
+        <ProductList items={products} />
+        <ProductListWithToggle title="Products HOC" items={products} />
       </div>
     </div>
-  );
-}
-
-// LATER: Let's say we got this component from a 3rd-party library, and can't change it. But we still want to add the 2 toggle functionalities to it
-function ProductList({ title, items }) {
-  return (
-    <ul className="list">
-      {items.map((product) => (
-        <ProductItem key={product.productName} product={product} />
-      ))}
-    </ul>
   );
 }
